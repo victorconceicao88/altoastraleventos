8ed85489f779991edf51953f0a73affef1684b3d
victor.conceicao <victor.engenhariasoftware@gmail.com>
2025-06-20 13:26:00 +0100

    atualizaçao

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,13 @@ import { getDatabase } from 'firebase/database';
 import { getAuth, setPersistence, browserLocalPersistence } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDPGwoIF7ReMQsjXGngZ86vuC1P2X0iV0E",
-  authDomain: "auto-astral-b5295.firebaseapp.com",
-  databaseURL: "https://auto-astral-b5295-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "auto-astral-b5295",
-  storageBucket: "auto-astral-b5295.firebasestorage.app",
-  messagingSenderId: "865984431676",
-  appId: "1:865984431676:web:1202dc70df895259c46539"
+  apiKey: "AIzaSyCX3kdISW1yVz-L5Ul2nUIWLBmOxIA-Ah4",
+  authDomain: "autoastralmesas.firebaseapp.com",
+  databaseURL: "https://autoastralmesas-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "autoastralmesas",
+  storageBucket: "autoastralmesas.firebasestorage.app",
+  messagingSenderId: "682428196172",
+  appId: "1:682428196172:web:2600c9709bcf0fce111be9"
 };
 
 const app = initializeApp(firebaseConfig);
